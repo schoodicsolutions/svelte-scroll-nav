642cdd430df8f3c7799063e3707b1826f77d766f
Ryan Prescott <rppgoat@gmail.com>
2023-01-05 18:38:12 -0500

    Change version, move all files to `lib`

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,12 +1,6 @@
 import { writable } from 'svelte/store';
 import { reserved } from './constants';
 
-
-interface SectionWeight {
-    name: string,
-    weight: number,
-}
-
 export const section = writable(reserved.top);
 export const sections = writable<Map<string, HTMLElement>>(new Map());
 export const linkClicked = writable(false);
